ef8a94309fff52b3b83fe859e721e26307d1d28f
Gian Carlo Mantuan <giancarlo.mdr@gmail.com>
2020-12-31 17:22:45 -0300

    minor fixes in Transacrion Repository

--- src/repositories/Transactions/TransactionRepository.ts
+++ src/repositories/Transactions/TransactionRepository.ts
@@ -4,6 +4,8 @@ import Transactions from '../../entities/Transactions';
 import ITransactionsRepository from './ITransactionsRepository';
 import ITransactionsDTO from './ITransactionsDTO';
 
+import { ErrorHandler } from '../../helpers/error';
+
 export default class TransactionsRepository implements ITransactionsRepository {
   private _transactionsRepository: Repository<Transactions>
 
@@ -13,7 +15,11 @@ export default class TransactionsRepository implements ITransactionsRepository {
 
   // Create a new Transaction
   public async add(transaction: ITransactionsDTO): Promise<Transactions[]> {
-    return await this._transactionsRepository.save(transaction)
+    try {
+      return await this._transactionsRepository.save(transaction)
+    } catch (error) {
+      throw new ErrorHandler(error.message, 500)
+    }
   }
 
 }
