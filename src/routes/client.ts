23b2f1cb2832c75a07bebc9a30a0abf5382cb8b3
Gian Carlo Mantuan <giancarlo.mdr@gmail.com>
2020-12-31 17:24:05 -0300

    add auth middleware in routes, add Joi validation in request

--- src/routes/client.ts
+++ src/routes/client.ts
@@ -1,13 +1,15 @@
 import {Router} from 'express';
 import ClientController from '../controllers/ClientController';
 import AuthController from '../controllers/AuthController';
+import {clientValidator} from '../middleware/validator'
 
 const router = Router();
 
 const clientController = new ClientController();
 const authController = new AuthController()
 
-router.post('/', clientController.save);
-router.post('/auth', authController.authenticate)
+router.post('/', clientValidator, clientController.save);
+
+router.post('/auth', clientValidator, authController.authenticate)
 
 export default router;
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import TransactionsController from '../controllers/TransactionsController'
 import {validateToken} from '../middleware/validateToken'
+import {transactionValitador} from '../middleware/validator'
 
 const router = Router();
 
 const transactionsController = new TransactionsController()
 
-router.post('/', validateToken, transactionsController.save);
+router.post('/', [validateToken, transactionValitador], transactionsController.save);
 
 export default router;
