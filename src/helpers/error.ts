471ed8cb61ef3709b0daf998848e9a040028d6c9
Gian Carlo Mantuan <giancarlo.mdr@gmail.com>
2020-12-31 22:03:17 -0300

    add cors, fix errorHandler

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -16,11 +16,10 @@ export async function handleError(error: any, res: Response) {
   const { statusCode, message } = error;
 
   if (isCelebrateError(error)) {
-    const errorMessage = error.details.get('body')
     return res.status(400).send({
       status: 'Request Validation Error',
       statusCode,
-      message: errorMessage
+      message: error.details.get('body')?.message
     });
   } else {
     res.status(statusCode).json({
